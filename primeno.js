09ba16bd22785b2f7128d6d37370e75b3ea5e90e
Kaushal Sain <kk1951999@gmail.com>
2024-03-02 10:33:41 +0530

    Prime Promblem

---
+++ primeno.js
@@ -0,0 +1,24 @@
+function kk(num){
+    if(num<=1){
+        return false;
+    }
+    if(num<=3){
+        return true;
+    }
+    if(num%2===0 || num%3===0){
+        return false;
+    }
+    for(var i=5;i*i<=num;i=i+6){
+        if(num%i!==0 || num%(i+2)!==0){
+        return false;
+        }
+    }
+    return true;
+}
+if(kk(num)){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
+}
